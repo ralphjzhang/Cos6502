d9e5038b47d7dd3a38aa0aed21aded53a5d2163a
Zhang Jun <ralph.j.zhang@gmail.com>
2021-02-08 17:55:59 +0800

    adde STA/X/Y series

---
+++ core/cos6502.cpp
@@ -0,0 +1,160 @@
+#include "cos6502.hpp"
+
+namespace cos6502 {
+
+
+s32 CPU::Execute(s32 cycles, Mem& memory) {
+    auto LoadRegister = [&cycles, &memory, this](Word addr, Byte& reg) {
+        reg = ReadByte(cycles, addr, memory);
+        LoadRegisterSetStatus(reg);
+    };
+
+    const s32 cyclesRequested = cycles;
+    while (cycles > 0) {
+        Byte ins = FetchByte(cycles, memory);
+        switch (ins) {
+            case INS_LDA_IM: {
+                A = FetchByte(cycles, memory);
+                LoadRegisterSetStatus(A);
+            } break;
+            case INS_LDX_IM: {
+                X = FetchByte(cycles, memory);
+                LoadRegisterSetStatus(X);
+            } break;
+            case INS_LDY_IM: {
+                Y = FetchByte(cycles, memory);
+                LoadRegisterSetStatus(Y);
+            } break;
+            case INS_LDA_ZP: {
+                Word addr = AddrZeroPage(cycles, memory);
+                LoadRegister(addr, A);
+            } break;
+            case INS_LDX_ZP: {
+                Word addr = AddrZeroPage(cycles, memory);
+                LoadRegister(addr, X);
+            } break;
+            case INS_LDY_ZP: {
+                Word addr = AddrZeroPage(cycles, memory);
+                LoadRegister(addr, Y);
+            } break;
+            case INS_LDA_ZPX: {
+                Word addr = AddrZeroPageXY(cycles, X, memory);
+                LoadRegister(addr, A);
+            } break;
+            case INS_LDX_ZPY: {
+                Word addr = AddrZeroPageXY(cycles, Y, memory);
+                LoadRegister(addr, X);
+            } break;
+            case INS_LDY_ZPX: {
+                Word addr = AddrZeroPageXY(cycles, X, memory);
+                LoadRegister(addr, Y);
+            } break;
+            case INS_LDA_ABS: {
+                Word addr = AddrAbsolute(cycles, memory);
+                LoadRegister(addr, A);
+            } break;
+            case INS_LDX_ABS: {
+                Word addr = AddrAbsolute(cycles, memory);
+                LoadRegister(addr, X);
+            } break;
+            case INS_LDY_ABS: {
+                Word addr = AddrAbsolute(cycles, memory);
+                LoadRegister(addr, Y);
+            } break;
+            case INS_LDA_ABSX: {
+                Word addr = AddrAbsoluteXY(cycles, X, memory);
+                LoadRegister(addr, A);
+            } break;
+            case INS_LDA_ABSY: {
+                Word addr = AddrAbsoluteXY(cycles, Y, memory);
+                LoadRegister(addr, A);
+            } break;
+            case INS_LDX_ABSY: {
+                Word addr = AddrAbsoluteXY(cycles, Y, memory);
+                LoadRegister(addr, X);
+            } break;
+            case INS_LDY_ABSX: {
+                Word addr = AddrAbsoluteXY(cycles, X, memory);
+                LoadRegister(addr, Y);
+            } break;
+            case INS_LDA_INDX: {
+                Word addr = AddrIndirectX(cycles, memory);
+                A = ReadByte(cycles, addr, memory);
+                LoadRegisterSetStatus(A);
+            } break;
+            case INS_LDA_INDY: {
+                Word addr = AddrIndirectY(cycles, memory);
+                A = ReadByte(cycles, addr, memory);
+                LoadRegisterSetStatus(A);
+            } break;
+            case INS_STA_ZP: {
+                Word addr = AddrZeroPage(cycles, memory);
+                WriteByte(A, cycles, addr, memory);
+            } break;
+            case INS_STX_ZP: {
+                Word addr = AddrZeroPage(cycles, memory);
+                WriteByte(X, cycles, addr, memory);
+            } break;
+            case INS_STY_ZP: {
+                Word addr = AddrZeroPage(cycles, memory);
+                WriteByte(Y, cycles, addr, memory);
+            } break;
+            case INS_STA_ABS: {
+                Word addr = AddrAbsolute(cycles, memory);
+                WriteByte(A, cycles, addr, memory);
+            } break;
+            case INS_STX_ABS: {
+                Word addr = AddrAbsolute(cycles, memory);
+                WriteByte(X, cycles, addr, memory);
+            } break;
+            case INS_STY_ABS: {
+                Word addr = AddrAbsolute(cycles, memory);
+                WriteByte(Y, cycles, addr, memory);
+            } break;
+            case INS_STA_ZPX: {
+                Word addr = AddrZeroPageXY(cycles, X, memory);
+                WriteByte(A, cycles, addr, memory);
+            } break;
+            case INS_STX_ZPY: {
+                Word addr = AddrZeroPageXY(cycles, Y, memory);
+                WriteByte(X, cycles, addr, memory);
+            } break;
+            case INS_STY_ZPX: {
+                Word addr = AddrZeroPageXY(cycles, X, memory);
+                WriteByte(Y, cycles, addr, memory);
+            } break;
+            case INS_STA_ABSX: {
+                Word addr = AddrAbsoluteXY(cycles, X, memory);
+                WriteByte(A, cycles, addr, memory);
+                --cycles; // TODO why need this?
+            } break;
+            case INS_STA_ABSY: {
+                Word addr = AddrAbsoluteXY(cycles, Y, memory);
+                WriteByte(A, cycles, addr, memory);
+                --cycles; // TODO why need this?
+            } break;
+            case INS_STA_INDX: {
+                Word addr = AddrIndirectX(cycles, memory);
+                WriteByte(A, cycles, addr, memory);
+            } break;
+            case INS_STA_INDY: {
+                Word addr = AddrIndirectY(cycles, memory);
+                WriteByte(A, cycles, addr, memory);
+                --cycles;
+            } break;
+
+            case INS_JSR: {
+                Word subAddr = FetchWord(cycles, memory);
+                memory.WriteWord(PC - 1, SP, cycles);
+                PC = subAddr;
+                --cycles;
+            } break;
+            default: {
+                throw "Instruction not implemented";
+            } break;
+        }
+    }
+    return cyclesRequested - cycles;
+}
+
+} // namespace cos6502
---
+++ core/cos6502.hpp
@@ -0,0 +1,185 @@
+#pragma once
+#include <stdio.h>
+#include <stdlib.h>
+
+namespace cos6502 
+{
+using Byte = unsigned char;
+using Word = unsigned short;
+
+using u32 = unsigned int;
+using s32 = signed int;
+
+struct Mem;
+struct CPU;
+}
+
+struct cos6502::Mem {
+    static constexpr u32 MAX_MEM = 1024 * 64;
+    Byte Data[MAX_MEM];
+
+    void Initialise() {
+        for (u32 i = 0; i < MAX_MEM; ++i)
+            Data[i] = 0;
+    }
+
+    Byte operator[](u32 address) const {
+        return Data[address];
+    }
+
+    Byte& operator[](u32 address) {
+        return Data[address];
+    }
+
+    void WriteWord(Word value, u32 address, s32& cycles) {
+        Data[address] = value & 0xFF;
+        Data[address + 1] = (value >> 8);
+        cycles -= 2;
+    }
+};
+
+struct cos6502::CPU {
+    Word PC;        // program counter
+    Word SP;        // stack poiter
+
+    Byte A, X, Y;   // registers
+
+    Byte C : 1;     // status flags
+    Byte Z : 1;
+    Byte I : 1;
+    Byte D : 1;
+    Byte B : 1;
+    Byte V : 1;
+    Byte N : 1;
+
+    void Reset(Mem& memory) {
+        PC = 0xFFFC;
+        SP = 0x0100;
+        C = Z = I = D = B = V = N = 0;
+        A = X = Y = 0;
+        memory.Initialise();
+    }
+
+    Byte FetchByte(s32& cycles, Mem const& memory) {
+        Byte data = memory[PC++];
+        --cycles;
+        return data;
+    }
+
+    Word FetchWord(s32& cycles, Mem const& memory) {
+        // 6502 is little endian;
+        Word data = memory[PC++];
+        data |= (memory[PC++] << 8);
+        cycles -= 2;
+        return data;
+    }
+
+    Byte ReadByte(s32& cycles, Word addr, Mem const& memory) {
+        Byte data = memory[addr];
+        --cycles;
+        return data;
+    }
+
+    Word ReadWord(s32& cycles, Word addr, Mem const& memory) {
+        Byte loByte = ReadByte(cycles, addr, memory);
+        Byte hiByte = ReadByte(cycles, addr + 1, memory);
+        return loByte | (hiByte << 8);
+    }
+
+    void WriteByte(Byte value, s32& cycles, Word addr, Mem& memory) {
+        memory[addr] = value;
+        --cycles;
+    }
+
+    void LoadRegisterSetStatus(Byte reg) {
+        Z = (reg == 0);
+        N = (reg & 0b10000000) > 0;
+    }
+
+    static constexpr Byte
+        // LDA
+        INS_LDA_IM = 0xA9,
+        INS_LDA_ZP = 0xA5,
+        INS_LDA_ZPX = 0xB5,
+        INS_LDA_ABS = 0xAD,
+        INS_LDA_ABSX = 0xBD,
+        INS_LDA_ABSY = 0xB9,
+        INS_LDA_INDX = 0xA1,
+        INS_LDA_INDY = 0xB1,
+        // LDX
+        INS_LDX_IM = 0xA2,
+        INS_LDX_ZP = 0xA6,
+        INS_LDX_ZPY = 0xB6,
+        INS_LDX_ABS = 0xAE,
+        INS_LDX_ABSY = 0xBE,
+        // LDY
+        INS_LDY_IM = 0xA0,
+        INS_LDY_ZP = 0xA4,
+        INS_LDY_ZPX = 0xB4,
+        INS_LDY_ABS = 0xAC,
+        INS_LDY_ABSX = 0xBC,
+        // STA
+        INS_STA_ZP = 0x85,
+        INS_STA_ZPX = 0x95,
+        INS_STA_ABS = 0x8D,
+        INS_STA_ABSX = 0x9D,
+        INS_STA_ABSY = 0x99,
+        INS_STA_INDX = 0x81,
+        INS_STA_INDY = 0x91,
+        // STX
+        INS_STX_ZP = 0x86,
+        INS_STX_ZPY = 0x96,
+        INS_STX_ABS = 0x8E,
+        // STY
+        INS_STY_ZP = 0x84,
+        INS_STY_ZPX = 0x94,
+        INS_STY_ABS = 0x8C,
+
+        INS_JSR = 0x20
+        ;
+
+    s32 Execute(s32 cycles, Mem& memory);
+
+    Word AddrZeroPage(s32& cycles, Mem const& memory) {
+        Byte zeroPageAddr = FetchByte(cycles, memory);
+        return zeroPageAddr;
+    }
+
+    Word AddrZeroPageXY(s32& cycles, Byte regXY, Mem const& memory) {
+        Byte addr = FetchByte(cycles, memory);
+        addr += regXY;
+        --cycles;
+        return addr;
+    }
+
+    Word AddrAbsolute(s32& cycles, Mem const& memory) {
+        Word addr = FetchWord(cycles, memory);
+        return addr;
+    }
+
+    Word AddrAbsoluteXY(s32& cycles, Byte regXY, Mem const& memory) {
+        Word absAddr = FetchWord(cycles, memory);
+        Word addr = absAddr + regXY;
+        if (addr - absAddr >= 0xFF) // crosses page boundary
+            --cycles;
+        return addr;
+    }
+
+    Word AddrIndirectX(s32& cycles, Mem const& memory) {
+        Byte zpAddr = FetchByte(cycles, memory);
+        zpAddr += X;
+        --cycles;
+        Word effectiveAddr = ReadWord(cycles, zpAddr, memory);
+        return effectiveAddr;
+    }
+
+    Word AddrIndirectY(s32& cycles, Mem const& memory) {
+        Byte zpAddr = FetchByte(cycles, memory);
+        Word effectiveAddr = ReadWord(cycles, zpAddr, memory);
+        Word effectiveAddrY = effectiveAddr + Y;
+        if (effectiveAddrY - effectiveAddr >= 0xFF) // crosses page boundary
+            --cycles;
+        return effectiveAddrY;
+    }
+};
+
